3ef176de4f318d67bd2aea16cedda20c526a939d
Tim <tim.terf@gmail.com>
2021-04-14 17:31:33 -0400

    updated domains

--- certbot/bundle.sh
+++ certbot/bundle.sh
@@ -2,3 +2,4 @@
 
 # needed for haproxy
 cat /etc/letsencrypt/live/catharinameints.com/fullchain.pem /etc/letsencrypt/live/catharinameints.com/privkey.pem > /etc/ssl/haproxy/catharinameints.com.pem
+cat /etc/letsencrypt/live/solana.market/fullchain.pem /etc/letsencrypt/live/solana.market/privkey.pem > /etc/ssl/haproxy/solana.market.pem
--- certbot/certs.sh
+++ certbot/certs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-certbot certonly --expand --webroot --webroot-path /var/www/html/letsencrypt -d catharinameints.com -d chi-cam.com -d nomilitary.org -d educationnotmilitarization.org -d email.chi-cam.com
+certbot certonly --expand --webroot --webroot-path /var/www/html/letsencrypt -d catharinameints.com -d solana.market
